9bc65ad2a48127bc572bfd45ef4ee52fb01c5e39
High End Patil <High End Patil@DESKTOP-5NN1PN8>
2021-10-23 14:25:09 +0530

    06-10-2021

---
+++ 07-10-2021/WebContent/comments in JS/JSFile.js
@@ -0,0 +1,20 @@
+//single line comment
+
+/**
+ * <=  document type  =>
+ * 
+ * java script code.
+ */
+
+function wishToAll()
+{
+	alert("Good Afternoon All....!");
+}
+
+document.getElementById("wish").onclick = wishToAll;
+
+/* 
+	in the above statements, we are defined a function
+	and calling that by using function onclick()
+	
+ */
---
+++ 07-10-2021/WebContent/java script basics and types/JSFile.js
@@ -0,0 +1,10 @@
+/**
+ * java script code.
+ */
+
+function wishToAll()
+{
+	alert("Good Afternoon All....!");
+}
+
+document.getElementById("wish").onclick = wishToAll;
